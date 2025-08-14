9437f419e09294a45b28b4097a21b10554cb796b
tttteeeee <pajacof994@mytaemin.com>
2025-08-14 16:10:17 +0300

    update env and connection string

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,9 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@assets': path.resolve(__dirname, './src/assets'),
     }
   }
 })
